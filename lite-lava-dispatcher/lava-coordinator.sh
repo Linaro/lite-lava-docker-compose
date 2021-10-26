057e0d7d8450063dc764cb82cffc1cd05c72f3ec
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2021-10-26 17:48:48 +0300

    lite-lava-dispatcher: Don't install lava-coordinator, rely on cointainerized
    Installing lava-coordinator via apt was effectively a quick stop-gap
    measure to get multinode jobs working in dockerized setup. However,
    apt package is quite dated (2020.12 currently) and pulls similarly
    dated lava-common package which recently started to actively conflict
    with other dispatcher code.
    
    OTOH, the (upstream) docker setup includes a container running
    coordinator. And real problem originally was actually getting
    a proper coordinator config on the dispatcher, installing the
    lava-coordinator package seemed like an easy way to do that.
    So, now we stop installing the whole package, and instead
    install just the config file pointing to a proper host in the
    docker-compose system.
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- lite-lava-dispatcher/lava-coordinator.sh
+++
@@ -1,6 +0,0 @@
-# LAVA Docker startup script for lava-coordinator.
-# This should be put in /root/entrypoint.d/ and relies on entrypoint.sh
-# processing from official LAVA Docker images.
-
-rm -f /var/run/lava-coordinator.pid
-/usr/bin/lava-coordinator --loglevel=DEBUG
